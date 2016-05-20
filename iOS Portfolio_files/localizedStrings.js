69e24f0894da1f2e3976a126a4dd9f8755383477
Steffen Frost <github@steffenfrost.com>
2016-05-20 11:46:55 -0700

    Adding my old web page from Internet Archive

---
+++ iOS Portfolio_files/localizedStrings.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 22:25:14 Jan 23, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 3:33:14 May 20, 2016.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+
+(function(){var strings={};strings['<b>The YouTube URL you entered is invalid.</b><br />Double-check the URL on YouTube, and then try again.']='<b>The YouTube URL you entered is invalid.</b><br />Double-check the URL on YouTube, and then try again.';strings['<b>You must be connected to the Internet to view the YouTube movie.</b>']='<b>You must be connected to the Internet to view the YouTube movie.</b>';RegisterWidgetStrings("com-apple-iweb-widget-YouTube",strings);})();
